f546a56c3a9aa08318554fb0058cd7c3f460bf43
itggot-lukas-wiklund <lukas.wiklund@itggot.se>
2017-05-08 10:59:09 +0200

    Added support for new pages

--- site/js/navigator.js
+++ site/js/navigator.js
@@ -1,7 +1,13 @@
 function goToPage(pageID) {
 	var home = document.getElementById("home");
+	var soda = document.getElementById("soda");
+	var candy = document.getElementById("candy");
+	var chips = document.getElementById("chips");
 	var cart = document.getElementById("cart");
 	home.classList.add("hidden");
+	soda.classList.add("hidden");
+	candy.classList.add("hidden");
+	chips.classList.add("hidden");
 	cart.classList.add("hidden");
 
 	var page = document.getElementById(pageID);
